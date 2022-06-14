9314aaf2aaeba32c7ee6be1053df1bfe6f05d8b1
hoangak47 <57388240+hoangak47@users.noreply.github.com>
2022-06-14 21:22:57 +0700

    update_URL

--- src/url/index.js
+++ src/url/index.js
@@ -1,2 +1,2 @@
-export const URL = process.env.REACT_APP_ZING_BASE_URL;
+export const URL = 'https://server-zingmp3.herokuapp.com/api/';
 // export const URL = process.env.REACT_APP_ZING_BASE_URL_2;
