3a40ba9c91f383febdc049a0d1ab7cae8e0b18d6
hoangak47 <57388240+hoangak47@users.noreply.github.com>
2022-07-01 10:25:20 +0700

    update

--- src/url/index.js
+++ src/url/index.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 export const URL = 'https://server-zing.vercel.app/api/';
-// export const URL = 'http://localhost:8000/api/';
-=======
- export const URL = 'https://server-zing.vercel.app/api/';
 //export const URL = 'http://localhost:8000/api/';
->>>>>>> cce377c74ed5bba09a0a2a8f8b4928764d1aa23b
